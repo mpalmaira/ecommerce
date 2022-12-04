51c63672dd3f657eb5e239c84ab99ffe72197f88
Meagan Palmaira <mmegcode@gmail.com>
2022-12-04 13:29:48 -0500

    added sanity clint

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { client } from "../lib/client";
 import { Product, FooterBanner, HeroBanner } from "../components";
 
 const Home = () => {
@@ -15,6 +16,21 @@ const Home = () => {
       <FooterBanner />
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: {
+      products,
+      bannerData,
+    },
+  };
 };
 
 export default Home;
