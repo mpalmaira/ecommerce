ec9475102072d4b80d2001f3ba97aa643e95e055
Meagan Palmaira <mmegcode@gmail.com>
2022-12-06 11:31:44 -0500

    final hook up sanity

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,17 @@ import React from "react";
 import { client } from "../lib/client";
 import { Product, FooterBanner, HeroBanner } from "../components";
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      {console.log(bannerData)}
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speaker of many variations</p>
       </div>
       <div className="products-container">
-        {["Product 1", "Product 2"].map((product) => product)}
+        {products?.map((product) => product.name)}
       </div>
       <FooterBanner />
     </>
@@ -19,10 +20,10 @@ const Home = () => {
 };
 
 export const getServerSideProps = async () => {
-  const query = '*[_type == "product]';
+  const query = '*[_type == "product"]';
   const products = await client.fetch(query);
 
-  const bannerQuery = '*[_type == "banner]';
+  const bannerQuery = '*[_type == "banner"]';
   const bannerData = await client.fetch(bannerQuery);
 
   return {
